aefa91d2747c6792ee778bb156b5e78d80263763
Felipe Moraes <em.felipe@gmail.com>
2021-11-20 02:16:19 -0300

    primeiro commit

---
+++ Ex202_image_transformation.py
@@ -0,0 +1,33 @@
+#<!---------------------------------------------------------------------------->
+#<!--                  IFSP - Instituto Federal de São Paulo                 -->
+#<!--                       Tópicos Avançados (TPA A6)                       -->
+#<!-- File       : Ex202_image_transformation.py                             -->
+#<!-- Description: Script to apply geometric transformations (rotation,      -->
+#<!--            : scale and translation) in image sequences                 -->
+#<!-- Author     : Fabricio Batista Narcizo (narcizo[at]itu[dot]dk)          -->
+#<!-- Information: No additional information                                 -->
+#<!-- Date       : 27/10/2021                                                -->
+#<!-- Change     : 27/10/2021 - Creation of this script                      -->
+#<!-- Review     : 27/10/2021 - Finalized                                    -->
+#<!---------------------------------------------------------------------------->
+
+__version__ = "$Revision: 2021102701 $"
+
+################################################################################
+import cv2
+import math
+import numpy as np
+
+########################################################################
+def affineTransformation(image, theta=0, t=(0, 0), s=1.):
+    """
+    This function return a 3x3 homogeneous transformation matrix that
+    represents a rotation followed by a translation transformation.
+    """
+    # TODO: Your code here
+
+    # Remove this command.
+    pass
+
+
+# TODO: Your code here
